62d0a5b0045fd881b63fee1b81c2d5145f0b75af
vvaks <vvaks@hortonworks.com>
2016-10-08 09:41:17 -0400

    entity lineage

--- src/main/java/com/hortonworks/nifi/AtlasLineageReportingTask.java
+++ src/main/java/com/hortonworks/nifi/AtlasLineageReportingTask.java
@@ -291,13 +291,15 @@ public class AtlasLineageReportingTask extends AbstractReportingTask {
         final LineageReferenceType[] inputs = {new LineageReferenceType(ingressPoint.getId()._getId().replace("[", "").replace("]", "").replace("\"", "").replace("\\", ""), jsonClass, version, typeName)};
         final LineageReferenceType[] outputs = {new LineageReferenceType(egressPoint.getId()._getId().replace("[", "").replace("]", "").replace("\"", "").replace("\\", ""), jsonClass, version, typeName)};
         */
+        Id[] inputs = {ingressPoint.getId()};
+        Id[] outputs = {egressPoint.getId()};
         final Referenceable nifiFlow = new Referenceable("nifi_flow");
         nifiFlow.set(AtlasClient.REFERENCEABLE_ATTRIBUTE_NAME, id);
         nifiFlow.set("flow_id", id);
         //nifiFlow.set("name", name+"_"+id+"_"+inputs[0].getId()+"_"+outputs[0].getId());
         nifiFlow.set("name", name+"_"+id+"_"+ingressPoint.getId()+"_"+egressPoint.getId());
-        nifiFlow.set("inputs", ingressPoint.getId());
-        nifiFlow.set("outputs", egressPoint.getId());
+        nifiFlow.set("inputs", inputs);
+        nifiFlow.set("outputs", outputs);
         nifiFlow.set("description", Arrays.toString(nifiLineage.toArray()));
         
         return nifiFlow;
